5640e0c023085ebd62dd5e8d03b50f62928b998e
samsepi101 <121718273+samsepi101@users.noreply.github.com>
2025-04-11 11:26:22 +0530

    Initial commit with Tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-base-200 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-500">Hello Tailwind + DaisyUI!</h1>
     </div>
+
   );
 }
 
